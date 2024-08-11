f31eef8575c9432f612d0c1c46e047a2192de351
Suyel Bishnu <s2301242@BCVFVFLU1WG7Air.home>
2024-08-11 19:27:07 +0300

    Change files

--- var/cache/dev/twig/7f/7ff271614bb519b76c62010714799bc3.php
+++ var/cache/dev/twig/7f/7ff271614bb519b76c62010714799bc3.php
@@ -53,22 +53,25 @@ class __TwigTemplate_dde83c5cc0cdd2120c0aca0f1ecd6b89 extends Template
         yield "\t\t</title>
 
 \t\t<link rel=\"icon\" href=\"data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 128 128%22><text y=%221.2em%22 font-size=%2296%22>⚫️</text><text y=%221.3em%22 x=%220.2em%22 font-size=%2276%22 fill=%22%23fff%22>sf</text></svg>\">
+\t\t<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css\">
+
 
 \t\t<link rel=\"stylesheet\" href=\"./css/styles.css\">
-\t\t<link rel=\"stylesheet\" href=\"/css/editStyles.css\"> 
+\t\t<link
+\t\trel=\"stylesheet\" href=\"/css/editStyles.css\">
 \t\t";
-        // line 15
+        // line 18
         yield "\t\t";
         yield from $this->unwrap()->yieldBlock('javascripts', $context, $blocks);
-        // line 16
-        yield "\t\t</head>
-\t\t<body>
-\t\t\t";
-        // line 18
+        // line 23
+        yield "\t</head>
+\t<body>
+\t\t";
+        // line 25
         yield from $this->unwrap()->yieldBlock('body', $context, $blocks);
-        // line 19
-        yield "\t\t</body>
-\t</html>
+        // line 26
+        yield "\t</body>
+</html>
 ";
         
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
@@ -91,20 +94,25 @@ class __TwigTemplate_dde83c5cc0cdd2120c0aca0f1ecd6b89 extends Template
         return; yield '';
     }
 
-    // line 15
+    // line 18
     public function block_javascripts($context, array $blocks = [])
     {
         $macros = $this->macros;
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "javascripts"));
 
+        // line 19
+        yield "\t\t\t<script src=\"https://cdn.jsdelivr.net/npm/sweetalert2@11\"></script>
+
+
+\t\t";
         
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
 
         return; yield '';
     }
 
-    // line 18
+    // line 25
     public function block_body($context, array $blocks = [])
     {
         $macros = $this->macros;
@@ -130,7 +138,7 @@ class __TwigTemplate_dde83c5cc0cdd2120c0aca0f1ecd6b89 extends Template
      */
     public function getDebugInfo()
     {
-        return array (  108 => 18,  95 => 15,  80 => 6,  70 => 19,  68 => 18,  64 => 16,  61 => 15,  53 => 8,  51 => 6,  44 => 1,);
+        return array (  116 => 25,  105 => 19,  98 => 18,  83 => 6,  73 => 26,  71 => 25,  67 => 23,  64 => 18,  53 => 8,  51 => 6,  44 => 1,);
     }
 
     public function getSourceContext()
@@ -145,16 +153,23 @@ class __TwigTemplate_dde83c5cc0cdd2120c0aca0f1ecd6b89 extends Template
 \t\t</title>
 
 \t\t<link rel=\"icon\" href=\"data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 128 128%22><text y=%221.2em%22 font-size=%2296%22>⚫️</text><text y=%221.3em%22 x=%220.2em%22 font-size=%2276%22 fill=%22%23fff%22>sf</text></svg>\">
+\t\t<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css\">
+
 
 \t\t<link rel=\"stylesheet\" href=\"./css/styles.css\">
-\t\t<link rel=\"stylesheet\" href=\"/css/editStyles.css\"> 
+\t\t<link
+\t\trel=\"stylesheet\" href=\"/css/editStyles.css\">
 \t\t{# <link rel=\"stylesheet\" href=\"/css/newStyles.css\"> #}
-\t\t{% block javascripts %}{% endblock %}
-\t\t</head>
-\t\t<body>
-\t\t\t{% block body %}{% endblock %}
-\t\t</body>
-\t</html>
+\t\t{% block javascripts %}
+\t\t\t<script src=\"https://cdn.jsdelivr.net/npm/sweetalert2@11\"></script>
+
+
+\t\t{% endblock %}
+\t</head>
+\t<body>
+\t\t{% block body %}{% endblock %}
+\t</body>
+</html>
 ", "base.html.twig", "/var/www/web/templates/base.html.twig");
     }
 }
--- var/cache/dev/twig/ad/ad1c2be71bb5b949d71107d953c9b88d.php
+++ var/cache/dev/twig/ad/ad1c2be71bb5b949d71107d953c9b88d.php
@@ -28,6 +28,7 @@ class __TwigTemplate_3792b563c6a3178a5526312fe4ec6b86 extends Template
         $this->blocks = [
             'title' => [$this, 'block_title'],
             'stylesheets' => [$this, 'block_stylesheets'],
+            'javascripts' => [$this, 'block_javascripts'],
             'body' => [$this, 'block_body'],
         ];
     }
@@ -59,7 +60,7 @@ class __TwigTemplate_3792b563c6a3178a5526312fe4ec6b86 extends Template
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "title"));
 
         // line 4
-        yield "Book index
+        yield "\tBook index
 ";
         
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
@@ -75,7 +76,7 @@ class __TwigTemplate_3792b563c6a3178a5526312fe4ec6b86 extends Template
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "stylesheets"));
 
         // line 8
-        yield "\t
+        yield "\t<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css\">
 ";
         
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
@@ -84,71 +85,113 @@ class __TwigTemplate_3792b563c6a3178a5526312fe4ec6b86 extends Template
     }
 
     // line 11
+    public function block_javascripts($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "javascripts"));
+
+        // line 12
+        yield "\t<script src=\"https://cdn.jsdelivr.net/npm/sweetalert2@11\"></script>
+\t<script>
+\t\tdocument.addEventListener('DOMContentLoaded', function () {
+document.querySelectorAll('.delete-btn').forEach(function (button) {
+button.addEventListener('click', function (event) {
+event.preventDefault();
+const form = button.closest('form');
+
+Swal.fire({
+title: 'Are you sure?',
+text: \"You won't be able to revert this!\",
+icon: 'warning',
+showCancelButton: true,
+confirmButtonColor: '#3085d6',
+cancelButtonColor: '#d33',
+confirmButtonText: 'Yes, delete it!',
+cancelButtonText: 'No, cancel!'
+}).then((result) => {
+if (result.isConfirmed) {
+form.submit();
+}
+});
+});
+});
+});
+\t</script>
+";
+        
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
+
+        return; yield '';
+    }
+
+    // line 40
     public function block_body($context, array $blocks = [])
     {
         $macros = $this->macros;
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
 
-        // line 12
-        yield "\t<h1>Book index</h1>
+        // line 41
+        yield "\t<h1>Book Index</h1>
 \t<table class=\"table\">
 \t\t<thead>
 \t\t\t<tr>
-\t\t\t\t<th>Title:</th>
-\t\t\t\t<th>Author:</th>
-\t\t\t\t<th>Year:</th>
-\t\t\t\t<th>Genre:</th>
-\t\t\t\t<th>Description:</th>
-\t\t\t\t<th>Actions:</th>
+\t\t\t\t<th>Title</th>
+\t\t\t\t<th>Author</th>
+\t\t\t\t<th>Year</th>
+\t\t\t\t<th>Genre</th>
+\t\t\t\t<th>Description</th>
+\t\t\t\t<th>Actions</th>
 \t\t\t</tr>
 \t\t</thead>
 \t\t<tbody>
 \t\t\t";
-        // line 25
+        // line 54
         $context['_parent'] = $context;
-        $context['_seq'] = CoreExtension::ensureTraversable((isset($context["books"]) || array_key_exists("books", $context) ? $context["books"] : (function () { throw new RuntimeError('Variable "books" does not exist.', 25, $this->source); })()));
+        $context['_seq'] = CoreExtension::ensureTraversable((isset($context["books"]) || array_key_exists("books", $context) ? $context["books"] : (function () { throw new RuntimeError('Variable "books" does not exist.', 54, $this->source); })()));
         foreach ($context['_seq'] as $context["_key"] => $context["book"]) {
-            // line 26
+            // line 55
             yield "\t\t\t\t<tr>
 \t\t\t\t\t<td>";
-            // line 27
-            yield Twig\Extension\EscaperExtension::escape($this->env, CoreExtension::getAttribute($this->env, $this->source, $context["book"], "title", [], "any", false, false, false, 27), "html", null, true);
+            // line 56
+            yield Twig\Extension\EscaperExtension::escape($this->env, CoreExtension::getAttribute($this->env, $this->source, $context["book"], "title", [], "any", false, false, false, 56), "html", null, true);
             yield "</td>
 \t\t\t\t\t<td>";
-            // line 28
-            yield Twig\Extension\EscaperExtension::escape($this->env, CoreExtension::getAttribute($this->env, $this->source, $context["book"], "author", [], "any", false, false, false, 28), "html", null, true);
+            // line 57
+            yield Twig\Extension\EscaperExtension::escape($this->env, CoreExtension::getAttribute($this->env, $this->source, $context["book"], "author", [], "any", false, false, false, 57), "html", null, true);
             yield "</td>
 \t\t\t\t\t<td>";
-            // line 29
-            yield Twig\Extension\EscaperExtension::escape($this->env, CoreExtension::getAttribute($this->env, $this->source, $context["book"], "publishingYear", [], "any", false, false, false, 29), "html", null, true);
+            // line 58
+            yield Twig\Extension\EscaperExtension::escape($this->env, CoreExtension::getAttribute($this->env, $this->source, $context["book"], "publishingYear", [], "any", false, false, false, 58), "html", null, true);
             yield "</td>
 \t\t\t\t\t<td>";
-            // line 30
-            yield Twig\Extension\EscaperExtension::escape($this->env, CoreExtension::getAttribute($this->env, $this->source, $context["book"], "genre", [], "any", false, false, false, 30), "html", null, true);
+            // line 59
+            yield Twig\Extension\EscaperExtension::escape($this->env, CoreExtension::getAttribute($this->env, $this->source, $context["book"], "genre", [], "any", false, false, false, 59), "html", null, true);
             yield "</td>
 \t\t\t\t\t<td>";
-            // line 31
-            yield Twig\Extension\EscaperExtension::escape($this->env, CoreExtension::getAttribute($this->env, $this->source, $context["book"], "description", [], "any", false, false, false, 31), "html", null, true);
+            // line 60
+            yield Twig\Extension\EscaperExtension::escape($this->env, CoreExtension::getAttribute($this->env, $this->source, $context["book"], "description", [], "any", false, false, false, 60), "html", null, true);
             yield "</td>
 \t\t\t\t\t<td>
-\t\t\t\t\t\t<button><a href=\"";
-            // line 33
-            yield Twig\Extension\EscaperExtension::escape($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("book_edit", ["id" => CoreExtension::getAttribute($this->env, $this->source, $context["book"], "id", [], "any", false, false, false, 33)]), "html", null, true);
-            yield "\">edit</a></button>
-\t\t\t\t\t\t<!-- Start of the form for delete button -->
+\t\t\t\t\t\t<button>
+\t\t\t\t\t\t\t<a href=\"";
+            // line 63
+            yield Twig\Extension\EscaperExtension::escape($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("book_edit", ["id" => CoreExtension::getAttribute($this->env, $this->source, $context["book"], "id", [], "any", false, false, false, 63)]), "html", null, true);
+            yield "\">edit</a>
+\t\t\t\t\t\t</button>
+
 \t\t\t\t\t\t<form action=\"";
-            // line 35
-            yield Twig\Extension\EscaperExtension::escape($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("book_delete", ["id" => CoreExtension::getAttribute($this->env, $this->source, $context["book"], "id", [], "any", false, false, false, 35)]), "html", null, true);
-            yield "\" method=\"post\" onsubmit=\"return confirm('Are you sure?');\">
+            // line 66
+            yield Twig\Extension\EscaperExtension::escape($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("book_delete", ["id" => CoreExtension::getAttribute($this->env, $this->source, $context["book"], "id", [], "any", false, false, false, 66)]), "html", null, true);
+            yield "\" method=\"post\">
 \t\t\t\t\t\t\t<input type=\"hidden\" name=\"_token\" value=\"";
-            // line 36
-            yield Twig\Extension\EscaperExtension::escape($this->env, $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->renderCsrfToken(("delete" . CoreExtension::getAttribute($this->env, $this->source, $context["book"], "id", [], "any", false, false, false, 36))), "html", null, true);
+            // line 67
+            yield Twig\Extension\EscaperExtension::escape($this->env, $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->renderCsrfToken(("delete" . CoreExtension::getAttribute($this->env, $this->source, $context["book"], "id", [], "any", false, false, false, 67))), "html", null, true);
             yield "\">
 \t\t\t\t\t\t\t<input type=\"hidden\" name=\"_method\" value=\"DELETE\">
-\t\t\t\t\t\t\t<button type=\"submit\">delete</button>
+\t\t\t\t\t\t\t<button type=\"submit\" class=\"delete-btn\">delete</button>
 \t\t\t\t\t\t</form>
-\t\t\t\t\t\t<!-- End of the form for delete button -->
 \t\t\t\t\t</td>
 \t\t\t\t</tr>
 \t\t\t";
@@ -156,17 +199,16 @@ class __TwigTemplate_3792b563c6a3178a5526312fe4ec6b86 extends Template
         $_parent = $context['_parent'];
         unset($context['_seq'], $context['_iterated'], $context['_key'], $context['book'], $context['_parent'], $context['loop']);
         $context = array_intersect_key($context, $_parent) + $_parent;
-        // line 44
+        // line 74
         yield "\t\t</tbody>
 \t</table>
 
-<p>
-\t<a href=\"";
-        // line 48
+\t<p>
+\t\t<a href=\"";
+        // line 78
         yield $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("book_new");
         yield "\" id=\"create-new-link\">Add New Book</a>
-</p>
-
+\t</p>
 ";
         
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
@@ -195,7 +237,7 @@ class __TwigTemplate_3792b563c6a3178a5526312fe4ec6b86 extends Template
      */
     public function getDebugInfo()
     {
-        return array (  166 => 48,  160 => 44,  146 => 36,  142 => 35,  137 => 33,  132 => 31,  128 => 30,  124 => 29,  120 => 28,  116 => 27,  113 => 26,  109 => 25,  94 => 12,  87 => 11,  78 => 8,  71 => 7,  62 => 4,  55 => 3,  38 => 1,);
+        return array (  209 => 78,  203 => 74,  190 => 67,  186 => 66,  180 => 63,  174 => 60,  170 => 59,  166 => 58,  162 => 57,  158 => 56,  155 => 55,  151 => 54,  136 => 41,  129 => 40,  95 => 12,  88 => 11,  79 => 8,  72 => 7,  63 => 4,  56 => 3,  39 => 1,);
     }
 
     public function getSourceContext()
@@ -203,24 +245,53 @@ class __TwigTemplate_3792b563c6a3178a5526312fe4ec6b86 extends Template
         return new Source("{% extends 'base.html.twig' %}
 
 {% block title %}
-Book index
+\tBook index
 {% endblock %}
 
 {% block stylesheets %}
-\t
+\t<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css\">
+{% endblock %}
+
+{% block javascripts %}
+\t<script src=\"https://cdn.jsdelivr.net/npm/sweetalert2@11\"></script>
+\t<script>
+\t\tdocument.addEventListener('DOMContentLoaded', function () {
+document.querySelectorAll('.delete-btn').forEach(function (button) {
+button.addEventListener('click', function (event) {
+event.preventDefault();
+const form = button.closest('form');
+
+Swal.fire({
+title: 'Are you sure?',
+text: \"You won't be able to revert this!\",
+icon: 'warning',
+showCancelButton: true,
+confirmButtonColor: '#3085d6',
+cancelButtonColor: '#d33',
+confirmButtonText: 'Yes, delete it!',
+cancelButtonText: 'No, cancel!'
+}).then((result) => {
+if (result.isConfirmed) {
+form.submit();
+}
+});
+});
+});
+});
+\t</script>
 {% endblock %}
 
 {% block body %}
-\t<h1>Book index</h1>
+\t<h1>Book Index</h1>
 \t<table class=\"table\">
 \t\t<thead>
 \t\t\t<tr>
-\t\t\t\t<th>Title:</th>
-\t\t\t\t<th>Author:</th>
-\t\t\t\t<th>Year:</th>
-\t\t\t\t<th>Genre:</th>
-\t\t\t\t<th>Description:</th>
-\t\t\t\t<th>Actions:</th>
+\t\t\t\t<th>Title</th>
+\t\t\t\t<th>Author</th>
+\t\t\t\t<th>Year</th>
+\t\t\t\t<th>Genre</th>
+\t\t\t\t<th>Description</th>
+\t\t\t\t<th>Actions</th>
 \t\t\t</tr>
 \t\t</thead>
 \t\t<tbody>
@@ -232,25 +303,26 @@ Book index
 \t\t\t\t\t<td>{{ book.genre }}</td>
 \t\t\t\t\t<td>{{ book.description }}</td>
 \t\t\t\t\t<td>
-\t\t\t\t\t\t<button><a href=\"{{ path('book_edit', {'id': book.id}) }}\">edit</a></button>
-\t\t\t\t\t\t<!-- Start of the form for delete button -->
-\t\t\t\t\t\t<form action=\"{{ path('book_delete', {'id': book.id}) }}\" method=\"post\" onsubmit=\"return confirm('Are you sure?');\">
+\t\t\t\t\t\t<button>
+\t\t\t\t\t\t\t<a href=\"{{ path('book_edit', {'id': book.id}) }}\">edit</a>
+\t\t\t\t\t\t</button>
+
+\t\t\t\t\t\t<form action=\"{{ path('book_delete', {'id': book.id}) }}\" method=\"post\">
 \t\t\t\t\t\t\t<input type=\"hidden\" name=\"_token\" value=\"{{ csrf_token('delete' ~ book.id) }}\">
 \t\t\t\t\t\t\t<input type=\"hidden\" name=\"_method\" value=\"DELETE\">
-\t\t\t\t\t\t\t<button type=\"submit\">delete</button>
+\t\t\t\t\t\t\t<button type=\"submit\" class=\"delete-btn\">delete</button>
 \t\t\t\t\t\t</form>
-\t\t\t\t\t\t<!-- End of the form for delete button -->
 \t\t\t\t\t</td>
 \t\t\t\t</tr>
 \t\t\t{% endfor %}
 \t\t</tbody>
 \t</table>
 
-<p>
-\t<a href=\"{{ path('book_new') }}\" id=\"create-new-link\">Add New Book</a>
-</p>
-
+\t<p>
+\t\t<a href=\"{{ path('book_new') }}\" id=\"create-new-link\">Add New Book</a>
+\t</p>
 {% endblock %}
+
 ", "booklist/index.html.twig", "/var/www/web/templates/booklist/index.html.twig");
     }
 }
